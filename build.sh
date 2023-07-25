e19dc6cb9d5fadb1f191118af11aafaf68636814
Masi Kajander <maska@cvx.fi>
2023-07-26 01:05:33 +0300

    Use portable path in build script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build . -t bofh-build-img
+docker build "$SCRIPT_DIR" -t bofh-build-img
 docker run --rm --mount type=bind,src="$SCRIPT_DIR",dst=/compile-dir bofh-build-img
 echo "Build succeeded."
